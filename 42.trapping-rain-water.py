03c22095d501144edc2e0e2b9f7f8ca766f303db
dassubhasis1759 <subhasis@rocketlane.com>
2025-08-14 11:28:43 +0530

    chore: push solved problems

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -7,6 +7,18 @@
 # @lc code=start
 class Solution:
     def trap(self, height: List[int]) -> int:
-        
+        l, r = 0, len(height) - 1
+        LMAX, RMAX = height[l], height[r]
+        res = 0
+        while l < r:
+            if LMAX < RMAX:
+                l += 1
+                LMAX = max(LMAX, height[l])
+                res += LMAX - height[l]
+            else:
+                r -= 1
+                RMAX = max(RMAX, height[r])
+                res += RMAX - height[r]
+        return res
 # @lc code=end
 
